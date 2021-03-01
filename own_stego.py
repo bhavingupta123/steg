86fb9496a437a150d8b2eead3371cc81b4bc0911
Bhavin Gupta <bhavin-cs031@dsce.edu.in>
2021-03-01 23:34:48 +0530

    added comments

--- own_stego.py
+++ own_stego.py
@@ -42,16 +42,19 @@ def encode(image_name, secret_data):
             # modify the least significant bit only if there is still data to store
             if data_index < data_len:
                 # least significant red pixel bit
-                pixel[0] = int(r[:-1] + binary_secret_data[data_index], 2)
+                pixel[0] = int(r[:-1] + binary_secret_data[data_index], 2) # r[:-1] all chars except last one
                 data_index += 1
+                #print("red")
             if data_index < data_len:
                 # least significant green pixel bit
-                pixel[1] = int(g[:-1] + binary_secret_data[data_index], 2)
+                pixel[1] = int(g[:-1] + binary_secret_data[data_index], 2)  # base 2 = binary
                 data_index += 1
+                #print("green")
             if data_index < data_len:
                 # least significant blue pixel bit
                 pixel[2] = int(b[:-1] + binary_secret_data[data_index], 2)
                 data_index += 1
+                #print("blue")
             # if data is encoded, just break out of the loop
             if data_index >= data_len:
                 break
@@ -66,17 +69,18 @@ def decode(image_name):
     for row in image:
         for pixel in row:
             r, g, b = to_bin(pixel)
-            binary_data += r[-1]
+            binary_data += r[-1] # read the last bit only
             binary_data += g[-1]
             binary_data += b[-1]
-
+    
+    # data is in form  rgb rgb rgb ....
     # split by 8-bits
-    all_bytes = [ binary_data[i: i+8] for i in range(0, len(binary_data), 8) ]
+    all_bytes = [ binary_data[i: i+8] for i in range(0, len(binary_data), 8) ] #grp 8 bits
     # convert from bits to characters
     decoded_data = ""
     for byte in all_bytes:
-        decoded_data += chr(int(byte, 2))
-        if decoded_data[-5:] == "=====":
+        decoded_data += chr(int(byte, 2)) #convert bits to int , convert to char
+        if decoded_data[-5:] == "=====": # if reach end then stop
             break
     return decoded_data[:-5]
 
